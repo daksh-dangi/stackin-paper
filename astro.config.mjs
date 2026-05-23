a72c1132871a6e438906b96d9407507da317213c
daksh <daksh.dangi@gmail.com>
2026-05-23 19:34:53 -0400

    power sampling article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://daksh-dangi.github.io',
   base: '/stacking-paper',
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
